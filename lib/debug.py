9082a58a9bf9920a43274ed375de987cc2b84634
+ <+>
2023-10-01 19:58:28 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -11,3 +11,31 @@ if __name__ == '__main__':
     session = Session()
 
     import ipdb; ipdb.set_trace()
+
+    # Inside the interactive debugging session, you can perform CRUD operations:
+    # For example, let's create a new game and its associated reviews:
+    
+    # Create a new Game
+    new_game = Game(name="Sample Game", platform="PC")
+
+    # Add reviews to the game
+    review1 = Review(title="Great Game!", content="I loved it.", game=new_game)
+    review2 = Review(title="Not so good", content="Could be better.", game=new_game)
+
+    # Add the game and reviews to the session
+    session.add(new_game)
+    session.add(review1)
+    session.add(review2)
+
+    # Commit the changes to the database
+    session.commit()
+
+    # Retrieve and print the game with its reviews
+    game_with_reviews = session.query(Game).filter_by(name="Sample Game").first()
+    if game_with_reviews:
+        print(f"Game: {game_with_reviews.name}, Platform: {game_with_reviews.platform}")
+        print("Reviews:")
+        for review in game_with_reviews.reviews:
+            print(f"  Title: {review.title}, Content: {review.content}")
+
+    # You can also update and delete records as needed within the session.
--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,25 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+Base = declarative_base()
 
-Base = declarative_base(metadata=metadata)
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    platform = Column(String)
+
+    # Define a one-to-many relationship with Review
+    reviews = relationship("Review", backref="game")
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    
+    # Define a foreign key relationship to Game
+    game_id = Column(Integer, ForeignKey('games.id'))
